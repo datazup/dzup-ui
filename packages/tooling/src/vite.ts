385464314e31d33d0121ab937aa87b96c617b438
ninel.hodzic <ninel.hodzic@gmail.com>
2026-07-02 22:24:09 +0200

    style(packages): format Vite lib entry resolution

--- packages/tooling/src/vite.ts
+++ packages/tooling/src/vite.ts
@@ -38,8 +38,8 @@ export function createLibConfig(options: CreateLibConfigOptions): UserConfig {
     alias = {},
   } = options
 
-  const resolvedEntry =
-    typeof entry === 'string'
+  const resolvedEntry
+    = typeof entry === 'string'
       ? { index: resolve(baseDir, entry) }
       : Object.fromEntries(Object.entries(entry).map(([k, v]) => [k, resolve(baseDir, v)]))
 
